eea695933361f259ebb3d4d7809f18773a25067f
Alexander <51765464+alechries@users.noreply.github.com>
2020-12-30 04:55:31 +0200

    Upd

--- AdminUser/settings.py
+++ AdminUser/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'AdminUser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'alechries',
-        'USER': 'alechries',
-        'PASSWORD': 'gaoq2ulc',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
