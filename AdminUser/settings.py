20743d95af6169f63ef6d569bfc486dfe964fed8
Alexander <51765464+alechries@users.noreply.github.com>
2021-01-01 18:46:42 +0200

    Database postgresql connection

--- AdminUser/settings.py
+++ AdminUser/settings.py
@@ -85,10 +85,18 @@ WSGI_APPLICATION = 'AdminUser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AdminUserExampleDjango',
+        'USER': 'AdminUserExampleDjango',
+        'PASSWORD': 'admin',
+        'HOST': '188.225.18.86',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
@@ -129,6 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, 'static')]
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
