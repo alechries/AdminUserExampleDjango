f0e75300315a3eda8188fccfa5fc4d1403b2b040
Alexander <51765464+alechries@users.noreply.github.com>
2021-01-06 13:27:00 +0200

    Forms

--- AdminUser/apps/blog/forms.py
+++ AdminUser/apps/blog/forms.py
@@ -7,4 +7,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'body_preview', 'body')
+        fields = ('category', 'title', 'body_preview', 'body')
--- AdminUser/apps/blog/models.py
+++ AdminUser/apps/blog/models.py
@@ -4,12 +4,24 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=15)
     author = models.ForeignKey('auth.User', verbose_name='Автор', on_delete=models.CASCADE)
-    body_preview = models.TextField(verbose_name='Превью содержания', null=True, max_length=140)
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.CASCADE, null=True,)
+    body_preview = models.TextField(verbose_name='Превью содержания', null=True, max_length=120)
     body = models.TextField(verbose_name='Содержание')
 
     def __str__(self):
-        return self.title
+        return f"{self.title} - {self.body_preview}"
 
     class Meta:
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
+
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=15)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
