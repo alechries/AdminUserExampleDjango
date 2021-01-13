8d0085374c87aa493e1e1a6e2bdbf1e2279e7629
Alexander <51765464+alechries@users.noreply.github.com>
2021-01-13 15:47:20 +0200

    Categories

--- AdminUser/apps/blog/urls.py
+++ AdminUser/apps/blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import BlogListView, BlogDetailView, post_new
+from .views import BlogDetailView, post_new, post_list, post_list_by_category
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
+    path('', post_list, name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post'),
     path('post/new/', post_new, name='post_new'),
+    path('category/<int:pk>/', post_list_by_category, name='post_by_category'),
+
 ]
--- AdminUser/apps/blog/views.py
+++ AdminUser/apps/blog/views.py
@@ -1,14 +1,22 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm
 
 
-class BlogListView(ListView):
-    model = Post  # Post <- models.Model
-    template_name = 'main/home.html'
-    queryset = Post.objects.filter().order_by('-pub_date')[:]
+def post_list(request):
+    categories = Category.objects.all()
+    posts = Post.objects.filter(pub_date__lte=timezone.datetime.now()).order_by('pub_date')
+
+    return render(request, 'main/home.html', {'posts': posts, 'categories': categories})
+
+
+def post_list_by_category(request, pk):
+    categories = Category.objects.all()
+    category = get_object_or_404(Category, pk=pk)
+    posts = Post.objects.filter(category=category).order_by('pub_date')
+    return render(request, 'main/home.html', {'posts': posts, 'categories': categories})
 
 
 class BlogDetailView(DetailView):
@@ -18,11 +26,11 @@ class BlogDetailView(DetailView):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.pub_date = timezone.now()
+            post.pub_date = timezone.datetime.now()
             post.save()
             return redirect('post', pk=post.pk)
     form = PostForm()
