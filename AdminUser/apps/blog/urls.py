ab4479040c95a66cde166987eea009f5b161f6eb
Alexander <51765464+alechries@users.noreply.github.com>
2021-01-20 14:27:12 +0200

    Pagination styles, edit/delete post, account posts list

--- AdminUser/apps/blog/urls.py
+++ AdminUser/apps/blog/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import BlogDetailView, post_new, post_list, post_list_by_category
+from .views import BlogDetailView, post_new, post_list, post_list_by_category, user_posts, post_del, post_edit
 
 urlpatterns = [
     path('', post_list, name='home'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post'),
     path('post/new/', post_new, name='post_new'),
+    path('post/edit/<int:pk>/', post_edit, name='post_edit'),
+    path('post/del/<int:pk>/', post_del, name='post_del'),
     path('category/<int:pk>/', post_list_by_category, name='post_by_category'),
-
+    path('post/my/', user_posts, name='user_posts')
 ]
--- AdminUser/apps/blog/views.py
+++ AdminUser/apps/blog/views.py
@@ -3,20 +3,46 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Post, Category
 from .forms import PostForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def home_page(request, posts, categories):
+    paginator = Paginator(posts, 6)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'main/home.html', {'posts': posts, 'categories': categories, 'page': page})
 
 
 def post_list(request):
     categories = Category.objects.all()
     posts = Post.objects.filter(pub_date__lte=timezone.datetime.now()).order_by('-pub_date')
-
-    return render(request, 'main/home.html', {'posts': posts, 'categories': categories})
+    return home_page(request, posts, categories)
 
 
 def post_list_by_category(request, pk):
     categories = Category.objects.all()
     category = get_object_or_404(Category, pk=pk)
     posts = Post.objects.filter(category=category).order_by('-pub_date')
-    return render(request, 'main/home.html', {'posts': posts, 'categories': categories})
+    return home_page(request, posts, categories)
+
+
+def user_posts(request):
+    user = request.user
+    posts = Post.objects.filter(pub_date__lte=timezone.datetime.now(), author=user).order_by('-pub_date')
+    paginator = Paginator(posts, 6)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'main/user_posts.html', {'posts': posts, 'page': page})
 
 
 class BlogDetailView(DetailView):
@@ -34,4 +60,26 @@ def post_new(request):
             post.save()
             return redirect('post', pk=post.pk)
     form = PostForm()
+    return render(request, 'main/post_edit.html', {'form': form})
+
+
+def post_del(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author == request.user:
+        post.delete()
+    return redirect('home')
+
+
+def post_edit(request, pk):
+    if pk:
+        post = get_object_or_404(Post, pk=pk)
+        if post.author != request.user:
+            return redirect('post', pk=post.pk)
+    else:
+        post = Post(author=request.user)
+
+    form = PostForm(request.POST or None, instance=post)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('home')
     return render(request, 'main/post_edit.html', {'form': form})
